7c9282c237d612f7a74c97a2bf40309da39c30fb
Babyganesh <109017980+Babyganesh@users.noreply.github.com>
2022-07-10 13:36:36 +0530

    Add files via upload

---
+++ Firefof.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Firefof {
+	String name="Firefox";
+	float version= 100.2f;
+	boolean visible= true;
+	int year= 1998;
+	char logo= 'm';
+	public static void main(String[] args) {
+		System.out.println("this is my first script"); 
+	}
+
+}
